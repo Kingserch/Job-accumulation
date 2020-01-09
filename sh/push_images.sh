c671b705a85f08de2cdc15d6ce53285fa3acf8af
Machinist <838915764@qq.com>
2020-01-09 16:18:09 +0800

    commit sh

---
+++ sh/push_images.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Author：king
+registry=127.0.0.1:5000
+
+    ### DO NOT MODIFY THE FOLLOWING PART, UNLESS YOU KNOW WHAT IT MEANS ###
+    echo_r () {
+        [ $# -ne 1 ] && return 0
+        echo -e "\033[31m$1\033[0m"
+    }
+    echo_g () {
+        [ $# -ne 1 ] && return 0
+        echo -e "\033[32m$1\033[0m"
+    }
+    echo_y () {
+        [ $# -ne 1 ] && return 0
+        echo -e "\033[33m$1\033[0m"
+    }
+    echo_b () {
+        [ $# -ne 1 ] && return 0
+        echo -e "\033[34m$1\033[0m"
+    }
+
+    usage() {
+        docker images
+        echo "Usage: $0 registry1:tag1 [registry2:tag2...]"
+    }
+
+    [ $# -lt 1 ] && usage && exit
+
+    echo_b "The registry server is $registry"
+
+    for image in "$@"
+    do
+        echo_b "Uploading $image..."
+        docker tag $image $registry/$image
+        docker push $registry/$image
+
+        docker rmi $registry/$image
+        echo_g "Done"
+    done
